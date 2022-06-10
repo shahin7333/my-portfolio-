dac2c507725617d325ddcc157087b9cac316fe09
shaparan shahin <shaparan7333@gmail.com>
2022-06-10 17:09:48 +0600

    typescript add-extra

--- src/Components/Blogs/Blogs.js
+++ src/Components/Blogs/Blogs.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Blogs = () => {
     return (
         <div>
-            <h2 className='text-xl text-center my-24 text-rose-500'>Comming Soon...</h2>
+            <h2 className='text-xl text-center my-24 text-rose-500'>Coming Soon...</h2>
         </div>
     );
 };
